77f5f83bf8752df9c09f88eda2081fa0e1f29098
Gleb Vorobyev <keepflooding@gmail.com>
2023-02-14 14:26:29 +0700

    add checkstyle and fix errors

--- app/src/main/java/hexlet/code/Cli.java
+++ app/src/main/java/hexlet/code/Cli.java
@@ -2,7 +2,7 @@ package hexlet.code;
 
 import java.util.Scanner;
 public class Cli {
-    public static void greeting(){
+    public static void greeting() {
         Scanner scanner = new Scanner(System.in);
 
         System.out.print("May I have your name? ");
